8372834e5509ca338b6e7d0f7810bfabd1cbc563
Seve0wns <55601725+Seve0wns@users.noreply.github.com>
2022-07-13 14:46:31 -0300

    ex02 S03

---
+++ semana03/exercicios/ex02/iteration.js
@@ -0,0 +1,17 @@
+
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+  ];
+  listaConvidados.forEach(name=>{
+    document.getElementById("name").innerHTML+=`<li>${name.nome} ${name.sobrenome}</li>`;
+  });
+  
