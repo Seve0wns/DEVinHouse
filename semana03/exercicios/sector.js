b2abdc80c1e6638607d42ae32a93c3d3ba5b4f9b
Seve0wns <55601725+Seve0wns@users.noreply.github.com>
2022-07-13 16:59:39 -0300

    ex04 S03

---
+++ semana03/exercicios/sector.js
@@ -0,0 +1,36 @@
+let primeiro={
+    nome: "Ryan",
+    sobrenome: "Azevedo",
+    setor: "Camarote",
+    idade: 23
+};
+let segundo={
+    nome: "Tony",
+    sobrenome: "Dias",
+    setor: "camarote",
+    idade: 29
+};
+let terceiro={
+    nome: "Rodrigo",
+    sobrenome: "Ramos",
+    setor: "pista",
+    idade: 15
+};
+let quarto={
+    nome: "Thiago",
+    sobrenome: "Freitas",
+    setor: "pista",
+    idade: 17
+};
+let quinto={
+    nome: "Marcos",
+    sobrenome: "Silva",
+    setor: "arquibancada",
+    idade: 19
+};
+let sexto={
+    nome: "Will",
+    sobrenome: "Mascarenhas",
+    setor: "arquibancada",
+    idade: 16
+};
