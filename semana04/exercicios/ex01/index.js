2ec3da9c7fd4dd9cbaba728655902819efbbd231
Seve0wns <55601725+Seve0wns@users.noreply.github.com>
2022-07-18 18:40:34 -0300

    ex04 S04

--- semana04/exercicios/ex01/index.js
+++ semana04/exercicios/ex01/index.js
@@ -1,8 +1,26 @@
-function addAcc(){
+function addAcc(list,acc){
+    list.innerHTML+=`<option value=${acc.id}>${acc.nome}</option>`;
+}
+function fillAcc(){
     const html=document.getElementById("accounts");
     contasClientes.forEach(acc => {
-        html.innerHTML+=`<option value=${acc.id}>${acc.nome}</option>`;
+        addAcc(html,acc);
     });
+}
+function withdraw(value,id){
+    const acc=contasClientes.find(e=>e.id===id);
+    if(value<=0){
+        alert("Valor inválido!");
+    }
+    else if(value>acc.saldo){
+        alert(`Saldo insuficiente!
+        Saldo atual: ${acc.saldo}R$`);
+    }
+    else{
+        acc.saldo-=value;
+        alert(`Saque efetuado com sucesso!
+        Novo saldo: ${acc.saldo}R$`);
+    }
 }
 const contasClientes = [
     {
