f37845751b7b4e2247c48f28215ce86db8363301
Seve0wns <55601725+Seve0wns@users.noreply.github.com>
2022-07-20 22:50:40 -0300

    styling

--- semana04/exercicios/ex01/index.js
+++ semana04/exercicios/ex01/index.js
@@ -1,3 +1,44 @@
+function leftzero(str) {//ajusta a quantidade de zeros à esquerda
+    while (str[0] === "0") {
+        str.shift();
+    }
+    if (!str.length) {//verifica se há algum digito além de 0
+        return null;
+    }
+    while (str.length < 3) {
+        str.unshift("0");
+    }
+    return str;
+}
+function build(str) {//molda a string de forma adequada a ser operada
+    if (str.length > 3) {//solução para backspace
+        str = str.substring(0, str.length - 2) + str.charAt(str.length - 1);
+        //retira os penúltimos dois caracteres da string
+    }
+    str = str.match(/\d/g);//pega apenas os digitos da string em forma de vetor
+    return leftzero(str);
+}
+function kiloSep(str) {//adiciona divisão de casas decimais e milhares
+    str.splice(2, 0, ".");//adiciona . para divisão para casas decimais
+    for (let i = 3, count = 1; i < str.length; i++, count++) {//adiciona virgulas para divisão de milhares
+        if (count === 4) {
+            str.splice(i, 0, ",");
+            count = 0;
+        }
+    }
+    return str;
+}
+function format(str) {
+    str = build(str);
+    if (!str) {
+        return "";
+    }
+    return kiloSep(str.reverse()).reverse().join("") + "R$";
+}
+function getFormat() {
+    let input = document.getElementById("value");
+    input.value = format(input.value);
+}
 function addAcc(list, acc) {
     list.innerHTML += `<option value=${acc.id}>${acc.nome}</option>`;
 }
@@ -19,7 +60,8 @@ function validateValues(elements) {
 }
 function withdraw(value, id) {
     id = parseInt(id);
-    value = parseFloat(value);
+    value = parseFloat(value.replaceAll(/,/g, ""));
+    console.log(value);
     const acc = contasClientes.find(e => e.id === id);
     if (value <= 0) {
         alert("Valor inválido!");
@@ -47,10 +89,10 @@ function deposit(value, id) {
         Novo saldo: ${acc.saldo}R$`);
     }
 }
-function validatePass(typedPass,accPass){
-    accPass=parseInt(accPass);
-    accPass=contasClientes.find(e=>e.id===accPass);
-    return typedPass===accPass.senha;
+function validatePass(typedPass, accPass) {
+    accPass = parseInt(accPass);
+    accPass = contasClientes.find(e => e.id === accPass);
+    return typedPass === accPass.senha;
 }
 function operate() {
     const elements = {
@@ -61,8 +103,8 @@ function operate() {
     if (validateValues([elements.operation, elements.value, elements.account])) {
         return;
     }
-    if(!validatePass(prompt("digite sua senha:"),elements.account.value)){
-       return alert("Senha inválida!");
+    if (!validatePass(prompt("digite sua senha:"), elements.account.value)) {
+        return alert("Senha inválida!");
     }
     if (elements.operation.value === "sacar") {
         withdraw(elements.value.value, elements.account.value);
@@ -91,3 +133,5 @@ const contasClientes = [
         senha: "03041997"
     }
 ];
+const regex = /^\d+\.\d{2}/;
+const hint = "0.00";
