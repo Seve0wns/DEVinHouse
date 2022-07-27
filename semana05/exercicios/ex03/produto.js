e72bf31ee59946546dc344a4531e853525a70619
Seve0wns <55601725+Seve0wns@users.noreply.github.com>
2022-07-26 23:09:37 -0300

    ex03 S05 revised

--- semana05/exercicios/ex03/produto.js
+++ semana05/exercicios/ex03/produto.js
@@ -3,4 +3,10 @@ class Produto {
     preco;
     emEstoque;
     quantidade;
+    constructor(nome, preco, emEstoque, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.emEstoque = emEstoque;
+        this.quantidade = quantidade;
+    }
 }
