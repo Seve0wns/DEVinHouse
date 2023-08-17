83ad363dab8fd31c3ab829573ce8ee893a841b85
Seve0wns <jhonathanhartmann@alunos.utfpr.edu.br>
2023-08-16 21:29:06 -0300

    changes

--- semana07/exercicios/keishokutei/src/App.jsx
+++ semana07/exercicios/keishokutei/src/App.jsx
@@ -1,36 +1,43 @@
 import { Footer, Header, Section } from "./Components";
 import "./App.css";
+import products from "/products.json";
 
-const products = [
-  {
-    name: "chilli&chips",
-    image: "/src/public/chilli-chips.jpg",
-    prepareTime: "00:05",
-    price: 25.99,
-    desc: "Uma porção de nachos acompanhada de molho chilli",
-    category: "aperitivo",
-  },
-];
-
+// const products = [
+//   {
+//     name: "chilli&chips",
+//     image: "/src/public/chilli-chips.jpg",
+//     prepareTime: "00:05",
+//     price: 25.99,
+//     desc: "Uma porção de nachos acompanhada de molho chilli",
+//     category: "aperitivo",
+//   },
+// ];
+function filterByCategory(array,category){
+  return array.filter(el=>el.category===category)
+}
 function App() {
   return (
     <>
       <Header />
       <Section
         title="Aperitivos"
-        products={products.filter(
-          (product) => product.category === "aperitivo"
-        )}
+        products={filterByCategory(products,"aperitivo")}
       />
       <Section
         title="Bebidas"
-        products={products.filter((product) => product.category === "bebida")}
+        products={filterByCategory(products,"bebida")}
       />
       <Section
         title="Bebidas alcoólicas"
-        products={products.filter(
-          (product) => product.category === "Bebuda alcoólica"
-        )}
+        products={filterByCategory(products,"bebida alc")}
+      />
+      <Section
+      title="Saladas"
+      products={filterByCategory(products,"salada")}
+      />
+      <Section
+      title="Sobremesas"
+      products={filterByCategory(products,"sobremesa")}
       />
       <Footer />
     </>
